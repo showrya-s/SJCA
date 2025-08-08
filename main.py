5bad30f1973a6123c88f55957099c1ce57e20b7b
showrya-s <showryareddy.ambati@gmail.com>
2025-08-08 15:20:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def main_page():
-    return render_template('main.html')  # your existing main page HTML file
+def main():
+    return render_template('main.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
